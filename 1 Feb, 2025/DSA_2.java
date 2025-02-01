6e2a318bf2eff317acf2a625cafa1bae5d2a4a73
Techwith-Aditya <160505538+Techwith-Aditya@users.noreply.github.com>
2025-02-01 17:15:04 +0530

    DSA_2.java

--- 1 Feb, 2025/DSA_2.java
+++ 1 Feb, 2025/DSA_2.java
@@ -1,5 +1,6 @@
 // Problem: Diagonal Tree Traversal
 
+// Isme hm current node ko phle queue me dalege, fir uska left check krege, agr uska left exist kr rha h to use queue me dalege...is current element ko hm list me add kr dege...aur current ko right node pe phucha dege...fir current k liye vhi sare steps repeat hoge...
 class Tree {
     public ArrayList<Integer> diagonal(Node root) {
         Queue<Node> q=new LinkedList<>();
